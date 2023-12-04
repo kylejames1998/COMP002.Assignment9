aa40233cbd1f25d618ff29c08efa6464711d2b6f
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2023-12-04 09:21:52 -0800

    answered questions in console logs

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("Being present for lectures and being able to ask questions when I was having trouble or not understanding a concept was a huge part of my success during this course.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("There really isn't anything I would change about this course other than to maybe add some peer reviews of code for bugs and errors.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("I thought I was going to learn a little bit more than I did, but I understand that with the course being short-term our time was limited.");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("Everything was very new to me. I had zero programming knowledge other than markup languages prior to this course.");
